4304355a69c366163e03856e386b86a420e6f1f1
GibletsofJesus <thecraggen@hotmail.co.uk>
2015-11-06 23:34:01 +0000

    yup

--- AdvTech2/Assets/MyStuff/Scripts/boxManager.cs
+++ AdvTech2/Assets/MyStuff/Scripts/boxManager.cs
@@ -1,82 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class boxManager : MonoBehaviour {
 
     public GameObject[] boxes;
+    public Text lengthText;
+    public Image GuiImage;
+    public Sprite play, pause, stop, fwd, back;
+    public Slider SliderObject;
+    public float updateTime = 0.01f;
+
     List<Vector3> pos = new List<Vector3>();
     List<Quaternion> rot = new List<Quaternion>();
-
+  
     List<List<Vector3>> posArray = new List<List<Vector3>>();
     List<List<Quaternion>> rotArray = new List<List<Quaternion>>();
 
     int scroll = 0;
-    bool pause;
+    bool isPaused;
+    float tick = 0;
 	// Use this for initialization
 	void Start () {
-	
+        StartCoroutine(record());
 	}
-	
+
+    IEnumerator record()
+    {
+        while (!isPaused)
+        {
+            updateVariables();
+            yield return new WaitForSeconds(updateTime);
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            /*pos.Clear();
-            rot.Clear();
-            for (int i = 0; i < boxes.Length; i++)
-            {
-                pos.Add(boxes[i].transform.position);
-                rot.Add(boxes[i].transform.rotation);
-            }*/
+            SliderObject.interactable = true;
+            SliderObject.maxValue = posArray.Count;
+            SliderObject.value = posArray.Count-1;
+            //lengthText.text = ""+(tick * updateTime);
+
             scroll = posArray.Count;
-            pause = !pause;
+            isPaused = !isPaused;
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
+            GuiImage.sprite = fwd;
             scroll++;
+            if (scroll >= posArray.Count)
+                scroll = posArray.Count -1;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
+            GuiImage.sprite = back;
             scroll--;
+            if (scroll < 1)
+                scroll = 1;
         }
-
-        if (pause)
+        
+        if (isPaused)
         {
+            GuiImage.sprite = pause;
             for (int i = 0; i < boxes.Length; i++)
             {
                 boxes[i].transform.position = posArray[scroll-1][i];
-                //boxes[i].transform.position = pos[i];
                 boxes[i].transform.rotation = rotArray[scroll-1][i];
-                //boxes[i].transform.rotation = rot[i];
             }
         }
         else
         {
-            updateVariables();
+            SliderObject.interactable = false;
+            GuiImage.sprite = play;
         }
-        //Add some code here so we're not calling this every frame.
     }
 
     void updateVariables()
     {
+        pos.Clear();
+        rot.Clear();
         for (int i = 0; i < boxes.Length; i++)
         {
             pos.Add(boxes[i].transform.position);
             rot.Add(boxes[i].transform.rotation);
         }
-
-        if (posArray.Count < 1000)
-        {
-            posArray.Add(pos);
-            rotArray.Add(rot);
-        }
-
-        //Grab variables for game objects
-        //Store in array
-
-
+        
+        posArray.Add(new List<Vector3>(pos));
+        rotArray.Add(new List<Quaternion>(rot));
+        tick++;
     }
+
+    public void setScroll(float newscroll)
+    {
+        scroll = (int)newscroll;
+    }
+
 }
