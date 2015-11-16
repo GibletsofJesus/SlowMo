b4bf9a80f3bc5cb59c929cfdf40a010b1c67dc30
GibletsofJesus <thecraggen@hotmail.co.uk>
2015-11-16 23:20:45 +0000

    jfkcljflscd

--- AdvTech2/Assets/MyStuff/Scripts/boxManager.cs
+++ AdvTech2/Assets/MyStuff/Scripts/boxManager.cs
@@ -15,11 +15,18 @@ public class boxManager : MonoBehaviour
 
     List<Vector3> pos = new List<Vector3>();
     List<Quaternion> rot = new List<Quaternion>();
+    List<Vector3> angVel = new List<Vector3>();
+    List<Vector3> vel = new List<Vector3>();
+    List<float> playbackTime = new List<float>();
+
     List<GameObject> physicsObjects = new List<GameObject>();
     List<GameObject> animatedObjects = new List<GameObject>();
 
     List<List<Vector3>> posArray = new List<List<Vector3>>();
     List<List<Quaternion>> rotArray = new List<List<Quaternion>>();
+    List<List<Vector3>> angVelArray = new List<List<Vector3>>();
+    List<List<Vector3>> velArray = new List<List<Vector3>>();
+    List<List<float>> playbackTimeArray = new List<List<float>>();
 
     int scroll = 0;
     bool isPaused, rewind;
@@ -27,7 +34,10 @@ public class boxManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //Start Couroutine
         StartCoroutine(record());
+
+        //Add objects to lists
         for (int i = 0; i < boxes.Length; i++)
         {
             if (boxes[i].gameObject.CompareTag("Phys"))
@@ -59,16 +69,20 @@ public class boxManager : MonoBehaviour
             SliderObject.interactable = true;
             SliderObject.maxValue = posArray.Count;
             SliderObject.value = posArray.Count-1;
-            //lengthText.text = ""+(tick * updateTime);
             rewind = false;
             scroll = posArray.Count;
             isPaused = !isPaused;
 
-            //phys reset goes here
-            for (int i = 0; i < physicsObjects.Count; i++)
+            if (!isPaused)
             {
-                Rigidbody test = physicsObjects[i].GetComponent<Rigidbody>();
-                test.velocity = Vector3.zero;
+                GuiImage.sprite = play;
+                StartCoroutine(record());
+            }
+
+            for (int i = 0; i < animatedObjects.Count; i++)
+            {
+                Animator anim = animatedObjects[i].GetComponent<Animator>();
+                animatedObjects[i].GetComponent<Animator>().StopPlayback();
             }
         }
         #endregion
@@ -82,26 +96,16 @@ public class boxManager : MonoBehaviour
                 scroll = 2;
             for (int i = 0; i < physicsObjects.Count; i++)
             {
-                physicsObjects[i].transform.position = posArray[scroll-1][i];
-                physicsObjects[i].transform.rotation = rotArray[scroll-1][i];
+                physicsObjects[i].transform.position = posArray[scroll - 1][i];
+                physicsObjects[i].transform.rotation = rotArray[scroll - 1][i];
+                physicsObjects[i].GetComponent<Rigidbody>().angularVelocity= angVelArray[scroll - 1][i];
+                physicsObjects[i].GetComponent<Rigidbody>().velocity = velArray[scroll - 1][i];
             }
-        }
-        #endregion
-
-        #region Scrub through function
-        if (isPaused)
-        {
-            GuiImage.sprite = pause;
-            for (int i = 0; i < physicsObjects.Count; i++)
+            for (int i = 0; i < animatedObjects.Count; i++)
             {
-                physicsObjects[i].transform.position = posArray[scroll-1][i];
-                physicsObjects[i].transform.rotation = rotArray[scroll-1][i];
+                //animatedObjects[i].GetComponent<Animator>().playbackTime = playbackTimeArray[scroll - 1][i];
             }
-        }
-        else
-        {
-            SliderObject.interactable = false;
-            GuiImage.sprite = play;
+            lengthText.text = "Current Tick:" + scroll + "\nTotal Ticks " + tick;
         }
         #endregion
 
@@ -109,7 +113,7 @@ public class boxManager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-
+            GuiImage.sprite = back;
             for (int i = 0; i < animatedObjects.Count; i++)
             {
                 Animator anim = animatedObjects[i].GetComponent<Animator>();
@@ -117,20 +121,36 @@ public class boxManager : MonoBehaviour
                 speed = speed * -1.0f;
                 anim.SetFloat("speed", speed);
             }
-            for (int i = 0; i < physicsObjects.Count; i++)
-            {
-                Rigidbody test = physicsObjects[i].GetComponent<Rigidbody>();
-                test.velocity = Vector3.zero;
-            }
             rewind = !rewind;
-            isPaused = rewind;
             scroll = posArray.Count;
             if (!rewind)
             {
-                StartCoroutine(record());
+                GuiImage.sprite = play;
             }
         }
+        #endregion
 
+        #region Scrub through function
+        if (isPaused)
+        {
+            StopCoroutine(record());
+            GuiImage.sprite = pause;
+            for (int i = 0; i < physicsObjects.Count; i++)
+            {
+                physicsObjects[i].transform.position = posArray[scroll - 1][i];
+                physicsObjects[i].transform.rotation = rotArray[scroll - 1][i];
+                physicsObjects[i].GetComponent<Rigidbody>().angularVelocity = angVelArray[scroll - 1][i];
+                physicsObjects[i].GetComponent<Rigidbody>().velocity = velArray[scroll - 1][i];
+            }
+            for (int i = 0; i < animatedObjects.Count; i++)
+            {
+                //animatedObjects[i].GetComponent<Animator>().playbackTime = playbackTimeArray[scroll - 1][i];
+            }
+        }
+        else
+        {
+            SliderObject.interactable = false;
+        }
         #endregion
     }
 
@@ -138,22 +158,33 @@ public class boxManager : MonoBehaviour
     {
         pos.Clear();
         rot.Clear();
+        angVel.Clear();
+        vel.Clear();
+        playbackTime.Clear();
         for (int i = 0; i < physicsObjects.Count; i++)
         {
             pos.Add(physicsObjects[i].transform.position);
             rot.Add(physicsObjects[i].transform.rotation);
+            angVel.Add(physicsObjects[i].GetComponent<Rigidbody>().angularVelocity);
+            vel.Add(physicsObjects[i].GetComponent<Rigidbody>().velocity);
         }
         for (int i = 0; i < animatedObjects.Count; i++)
         {
+            playbackTime.Add(animatedObjects[i].GetComponent<Animator>().playbackTime);
         }
 
         posArray.Add(new List<Vector3>(pos));
         rotArray.Add(new List<Quaternion>(rot));
+        angVelArray.Add(new List<Vector3>(angVel));
+        velArray.Add(new List<Vector3>(vel));
+        playbackTimeArray.Add(new List<float>(playbackTime));
+        lengthText.text = "Current Tick:" + scroll + "\nTotal Ticks " + tick;
         tick++;
     }
 
     public void setScroll(float newscroll)
     {
         scroll = (int)newscroll;
+        lengthText.text = "Current Tick:" + scroll + "\nTotal Ticks " + tick;
     }
 }
